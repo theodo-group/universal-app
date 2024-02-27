49b1ce19b992115b0c126b178e5401d1ccbcec4d
rsamuel12 <rohansamm@gmail.com>
2024-02-27 14:18:40 +0000

    refactor: remove styled function from UI components

--- packages/frontend/ui/src/index.tsx
+++ packages/frontend/ui/src/index.tsx
@@ -1,4 +1,2 @@
-export { Row } from "./layout";
 export { theme } from "./tailwind/theme";
-export { A, H1, P, Text, TextLink } from "./typography";
-export { View } from "./view";
+export { A, H1, P, TextLink } from "./typography";
