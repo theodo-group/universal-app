b2b467c7901cb9aa126f50ff1d843bd4f711244c
Francisco-Costa98 <franciscomffcosta@hotmail.com>
2024-03-05 10:42:20 +0000

    added base navigation package

--- packages/frontend/providers/src/index.tsx
+++ packages/frontend/providers/src/index.tsx
@@ -3,3 +3,5 @@ import { SafeArea } from "./safe-area";
 export function Provider({ children }: { children: React.ReactNode }) {
   return <SafeArea>{children}</SafeArea>;
 }
+
+export { useNavigation } from "./navigation";
