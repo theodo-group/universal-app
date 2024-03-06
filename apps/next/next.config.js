cb1fcb8b6ba539169f4b91fa536c17660b7c89ef
rsamuel12 <rohansamm@gmail.com>
2024-03-06 12:03:33 +0000

    feat: setup SVG config for Next app

--- apps/next/next.config.js
+++ apps/next/next.config.js
@@ -19,6 +19,14 @@ const nextConfig = {
     "react-native-gesture-handler",
     "react-native-css-interop",
   ],
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      issuer: /\.[jt]sx?$/,
+      use: ["@svgr/webpack"],
+    });
+    return config;
+  },
 };
 
 module.exports = withExpo(nextConfig);
