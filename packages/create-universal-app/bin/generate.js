e7410bc0348734764aa8628a4af3f4eb8215b4a6
rsamuel12 <rohansamm@gmail.com>
2024-03-07 15:37:16 +0000

    feat: add universal app generator script

---
+++ packages/create-universal-app/bin/generate.js
@@ -0,0 +1,51 @@
+#!/usr/bin/env node
+import { spawn } from "child_process";
+import fs from "fs-extra";
+import ora from "ora";
+import path from "path";
+
+const repoURL = "https://github.com/theodo-group/universal-app.git";
+const projectName = process.argv[2];
+
+if (!projectName) {
+  console.error("Please provide a project name as an argument.");
+  process.exit(1);
+}
+
+const projectDir = path.join(process.cwd(), projectName);
+
+const runAsyncProcess = async (spawnFunction) => {
+  return new Promise((resolve, reject) => {
+    spawnFunction()
+      .on("close", (code) => {
+        if (code === 0) {
+          resolve();
+        } else {
+          reject(new Error("Failed to clone repository"));
+        }
+      })
+      .on("error", (err) => {
+        reject(err);
+      });
+  });
+};
+
+(async () => {
+  try {
+    if (fs.existsSync(projectDir)) {
+      console.error(`Directory "${projectDir}" already exists.`);
+      return;
+    }
+
+    const cloning = ora(`Cloning repository to "${projectDir}"...`).start();
+    await runAsyncProcess(() => spawn("git", ["clone", repoURL, projectDir]));
+    cloning.stop();
+
+    // TODO: Optionally remove unnecessary files in the future (e.g., graphql or REST)
+    // await fs.remove(path.join(projectDir, '.git'));
+
+    console.log("Project generated successfully!");
+  } catch (err) {
+    console.error(err);
+  }
+})();
