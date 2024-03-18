4f62c928e2482aca12cec2023e88d16cb92bef4e
Claire Squires <claires@theodo.co.uk>
2024-03-18 12:17:59 +0000

    refactor: add features dir, move user + home

--- packages/frontend/core/src/index.ts
+++ packages/frontend/core/src/index.ts
@@ -1,5 +1,5 @@
 export { Provider, useNavigation } from "providers";
 export type { RootStackParamList, RootTabParamList } from "providers";
 
-export { HomeScreen } from "./home/screen";
-export { UserDetailScreen } from "./user/detail-screen";
+export { HomeScreen } from "./features/home/screen";
+export { UserDetailScreen } from "./features/user/detail-screen";
