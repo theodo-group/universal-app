0cdce986815a20ee95626311769f6850d36cc3f2
Claire Squires <claires@theodo.co.uk>
2024-03-19 14:32:45 +0000

    feat: create login screen

---
+++ packages/frontend/core/src/features/auth/login/screen.tsx
@@ -0,0 +1,29 @@
+import { A, Button, H1, P } from "@frontend/design-system";
+import { FormField } from "features/auth/components/FormField";
+import { LOGIN_NAME, LOGIN_WITH, SIGNUP_NAME, toTitleCase } from "features/auth/login/constants";
+
+import { View } from "react-native";
+
+export const LoginScreen = () => {
+  return (
+    <View className="flex min-h-full flex-1 flex-col justify-center bg-white px-6 py-12 lg:px-8">
+      <View className="sm:mx-auto sm:w-full sm:max-w-sm">
+        <H1>{toTitleCase(LOGIN_NAME)} to your account</H1>
+        <FormField placeholder={toTitleCase(LOGIN_WITH)} />
+        <FormField placeholder="Password" />
+        <View className="mb-1 flex items-end">
+          <A href="#" className="font-semibold text-indigo-600 hover:text-indigo-500">
+            Forgot password?
+          </A>
+        </View>
+        <Button.primary onPress={() => {}} text="Login" />
+        <View className="flex flex-row items-center justify-center gap-2">
+          <P>Don't have an account?</P>
+          <A href="#" className="font-semibold text-indigo-600 hover:text-indigo-500">
+            {toTitleCase(SIGNUP_NAME)}
+          </A>
+        </View>
+      </View>
+    </View>
+  );
+};
--- packages/frontend/core/src/index.ts
+++ packages/frontend/core/src/index.ts
@@ -1,5 +1,6 @@
 export { Provider, useNavigation } from "providers";
 export type { RootStackParamList, RootTabParamList } from "providers";
 
+export { LoginScreen } from "./features/auth/login/screen";
 export { HomeScreen } from "./features/home/screen";
 export { UserDetailScreen } from "./features/user/detail-screen";
