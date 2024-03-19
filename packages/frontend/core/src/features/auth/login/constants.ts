60d6a47f4ecdc1dd3e50f009368d62df7dd0e550
Claire Squires <claires@theodo.co.uk>
2024-03-19 14:33:08 +0000

    feat: create constants for auth words

---
+++ packages/frontend/core/src/features/auth/login/constants.ts
@@ -0,0 +1,7 @@
+export const LOGIN_WITH = "username";
+export const LOGIN_NAME = "login";
+export const SIGNUP_NAME = "create account";
+
+export const toTitleCase = (str: string): string => {
+  return str.charAt(0).toUpperCase() + str.slice(1);
+};
