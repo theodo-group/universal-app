aaa4d0850014c4e971d1e7de7fd4abd81a760aac
Claire Squires <claires@theodo.co.uk>
2024-03-19 16:11:58 +0000

    refactor: move constants to utils, rename function to capatalize

--- packages/frontend/core/src/features/auth/utils/constants.ts
+++ packages/frontend/core/src/features/auth/utils/constants.ts
@@ -2,6 +2,6 @@ export const LOGIN_WITH = "username";
 export const LOGIN_NAME = "login";
 export const SIGNUP_NAME = "create account";
 
-export const toTitleCase = (str: string): string => {
+export const capatalize = (str: string): string => {
   return str.charAt(0).toUpperCase() + str.slice(1);
 };
