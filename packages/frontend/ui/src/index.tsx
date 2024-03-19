43a9135aff73110a436eb990025c58066726682b
Claire Squires <claires@theodo.co.uk>
2024-03-19 14:32:08 +0000

    feat: export button

--- packages/frontend/ui/src/index.tsx
+++ packages/frontend/ui/src/index.tsx
@@ -1,5 +1,7 @@
+export { Button } from "./components/button";
 export { Card } from "./components/card";
 export { Image } from "./components/image";
 export { List } from "./components/list";
+export { Pressable } from "./pressable";
 export { theme } from "./tailwind/theme";
 export { A, H1, P, TextLink } from "./typography";
