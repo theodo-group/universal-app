7b89730790c4f040ae45d864d3ac3634adabf1a4
Claire Squires <claires@theodo.co.uk>
2024-03-20 12:55:20 +0000

    fix: typo

--- packages/frontend/core/src/features/auth/login/screen.tsx
+++ packages/frontend/core/src/features/auth/login/screen.tsx
@@ -1,6 +1,6 @@
 import { A, Button, FormField, H1, P } from "@frontend/design-system";
 import { LOGIN_NAME, LOGIN_WITH, SIGNUP_NAME } from "features/auth/utils/constants";
-import { capatalize } from "features/auth/utils/helpers";
+import { capitalize } from "features/auth/utils/helpers";
 
 import { View } from "react-native";
 
@@ -8,8 +8,8 @@ export const LoginScreen = () => {
   return (
     <View className="flex min-h-full flex-1 flex-col justify-center bg-white px-6 py-12 lg:px-8">
       <View className="sm:mx-auto sm:w-full sm:max-w-sm">
-        <H1>{capatalize(LOGIN_NAME)} to your account</H1>
-        <FormField placeholder={capatalize(LOGIN_WITH)} />
+        <H1>{capitalize(LOGIN_NAME)} to your account</H1>
+        <FormField placeholder={capitalize(LOGIN_WITH)} />
         <FormField placeholder="Password" />
         <View className="mb-1 flex items-end">
           <A href="#" className="font-semibold text-indigo-600 hover:text-indigo-500">
@@ -20,7 +20,7 @@ export const LoginScreen = () => {
         <View className="flex flex-row items-center justify-center gap-2">
           <P>Don't have an account?</P>
           <A href="#" className="font-semibold text-indigo-600 hover:text-indigo-500">
-            {capatalize(SIGNUP_NAME)}
+            {capitalize(SIGNUP_NAME)}
           </A>
         </View>
       </View>
